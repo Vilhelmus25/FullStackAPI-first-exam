87167fbaa405dcffff49d7f8e317855502b2adba
Vilhelmus25 <vorvie@gmail.com>
2021-07-05 15:38:05 +0200

    fourth task done

--- task-04/jsonDB.js
+++ task-04/jsonDB.js
@@ -1,12 +1,15 @@
 /**
  * 1. A fájlok kezeléséhez az fs modul promise alapú verzióját használd.
  */
+const fsp = require('fs').promises
 
 /**
  * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
  * modul join metódusának segítségével.
  */
-const jsonPath = '';
+const { join } = require('path')
+
+const jsonPath = join(__dirname, 'db', 'products.json');
 
 /**
  * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
@@ -14,7 +17,8 @@ const jsonPath = '';
  * @returns objektumok tömbje
  */
 const getList = async () => {
-    //
+    const fileContent = await fsp.readFile(jsonPath, 'utf8')
+    return JSON.parse(fileContent)
 };
 
 /**
@@ -23,8 +27,8 @@ const getList = async () => {
  * 
  * @returns a termékek objektumai egy tömbben
  */
- const getAll = async () => {
-     //
+const getAll = async () => {
+    return await getList()
 };
 
 module.exports = {
